607f54bdb28b1ea6c7d28b57a394187fef351dd2
Neo Wang <43394822+nwatx@users.noreply.github.com>
2023-02-18 19:51:03 -0800

    .

--- tailwind-react-chrome-extension-template/src/pages/Popup/Popup.jsx
+++ tailwind-react-chrome-extension-template/src/pages/Popup/Popup.jsx
@@ -3,12 +3,50 @@ import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
+  const [text, setText] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
+
+  const handleSubmit = () => {
+    setMessages([...messages, { text, isUser: true }]);
+    setText('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header h-full">
         <div className="p-3">
-          <h1>Past Searches</h1>
+          <h1>Let's Talk About This Article</h1>
         </div>
+        <div className="my-auto overflow-y-scroll">
+          <div className="flex flex-col">
+            {messages.map((message, index) => (
+              <div
+                className={`
+              flex flex-row text-sm w-auto my-1 p-2 rounded-md overflow-y-auto
+              ${
+                message.isUser
+                  ? 'bg-blue-400 justify-self-end'
+                  : 'bg-gray-400 justify-self-start'
+              }`}
+                key={index}
+              >
+                {message.text}
+              </div>
+            ))}
+          </div>
+        </div>
+        <input
+          className="justify-self-end text-sm text-black m-2 px-2"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onSubmit={() => handleSubmit()}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmit();
+            }
+          }}
+        ></input>
       </header>
     </div>
   );
