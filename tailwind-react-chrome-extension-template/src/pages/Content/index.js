bdae2d506690710a156d9582506e781282c6242c
Neo Wang <43394822+nwatx@users.noreply.github.com>
2023-02-18 16:58:45 -0800

    Update index.js

--- tailwind-react-chrome-extension-template/src/pages/Content/index.js
+++ tailwind-react-chrome-extension-template/src/pages/Content/index.js
@@ -8,5 +8,16 @@ printLine("Using the 'printLine' function from the Print Module");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   const text = request.text;
   const highlightedElement = document.getSelection();
-  console.log(highlightedElement);
+  // we will insert it before the paragraph
+  const insert_node = highlightedElement.anchorNode.parentElement;
+
+  // now, inject a box into the DOM just above the highlighted text
+  // don't use absolute positioning because it will be relative to the
+  // highlighted text, which will change as the user scrolls
+  const box = document.createElement('p');
+  box.style.width = '100%';
+  box.style.height = '100%';
+  box.innerText = 'This is a box';
+
+  insert_node.insertBefore(box, highlightedElement.anchorNode);
 });
